bba668c273c8848439656a0c5189055d987b20b5
Etienne <eti@naude.dev>
2022-02-13 03:26:33 +1300

    added empty config file

---
+++ conf.h
@@ -0,0 +1,16 @@
+// LED SETTINGS
+#define INNER_PIN   1
+#define INNER_COUNT 13
+#define OUTER_PIN   2
+#define OUTER_NUM   17
+
+
+// WIFI SETTINGS
+#define WIFI_SSID         ""    
+#define WIFI_PASS         ""
+#define APP_KEY           ""      // Should look like "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
+#define APP_SECRET        ""      // Should look like "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
+#define LIGHT_ID          ""      // Should look like "5dc1564130xxxxxxxxxxxxxx"
+
+
+#define BAUD_RATE         115200   
