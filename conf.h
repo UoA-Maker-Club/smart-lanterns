018fcaf1f4750c028d8338569ab02c4b2d77086c
Etienne <eti@naude.dev>
2022-04-21 20:57:55 +1200

    Updated defualt config

--- conf.h
+++ conf.h
@@ -1,16 +1,23 @@
+#define BAUD_RATE         115200
+
 // LED SETTINGS
-#define INNER_PIN   1
-#define INNER_COUNT 13
-#define OUTER_PIN   2
-#define OUTER_NUM   17
+#define LED_PIN   1
+#define LED_COUNT 30
 
 
 // WIFI SETTINGS
-#define WIFI_SSID         ""    
+#define WIFI_SSID         ""
 #define WIFI_PASS         ""
+
+#define LIGHT_ID          ""      // Should look like "5dc1564130xxxxxxxxxxxxxx"
 #define APP_KEY           ""      // Should look like "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
 #define APP_SECRET        ""      // Should look like "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
-#define LIGHT_ID          ""      // Should look like "5dc1564130xxxxxxxxxxxxxx"
 
+#define HUE 200
+#define PATTERN 1
 
-#define BAUD_RATE         115200   
+/*
+  0 -> Trail
+  1 -> rainbow trail
+  2 -> still
+*/
