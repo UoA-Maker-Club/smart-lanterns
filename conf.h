dc51b1f83060a0d89ebe1ab9b9fb13930dc573bf
Etienne <eti@naude.dev>
2022-07-20 01:05:52 +1200

    update config

--- conf.h
+++ conf.h
@@ -1,17 +1,19 @@
 #define BAUD_RATE         115200
 
 // LED SETTINGS
-#define LED_PIN   1
+#define LED_PIN   D4
 #define LED_COUNT 30
 
 
 // WIFI SETTINGS
-#define WIFI_SSID         ""
-#define WIFI_PASS         ""
+#define WIFI_SSID         "UoA-Unleash"
+#define WIFI_PASS         "UoA_Unl3ash"
 
-#define LIGHT_ID          ""      // Should look like "5dc1564130xxxxxxxxxxxxxx"
-#define APP_KEY           ""      // Should look like "de0bxxxx-1x3x-4x3x-ax2x-5dabxxxxxxxx"
-#define APP_SECRET        ""      // Should look like "5f36xxxx-x3x7-4x3x-xexe-e86724a9xxxx-4c4axxxx-3x3x-x5xe-x9x3-333d65xxxxxx"
+// Sinric Pro
+#define APP_KEY           ""
+#define APP_SECRET        ""
+#define LIGHT_ID          ""
+
 
 #define HUE 200
 #define PATTERN 1
